86970f294861ecfae2a202b14632edf4f691e66d
kumarvivekqa <cskumar.vivek@gmail.com>
2025-09-01 00:24:25 +0530

    31th aug 2025

--- src/ex_02_Java_Basics/lab016_Print_table.java
+++ src/ex_02_Java_Basics/lab016_Print_table.java
@@ -6,6 +6,17 @@ public class lab016_Print_table {
         int a = 5;
         int b = 6;
 
-        System.out.printf("%d * %d = %d",a,b,a*b);
+        System.out.printf("%d * %d = %d",a,b,a*b).println();
+        //Another method
+        int c = 5;
+        System.out.printf("%d*1=%d",c,c*1).println();
+        System.out.printf("%d*2=%d",c,c*2).println();
+        System.out.printf("%d*3=%d",c,c*3).println();
+        System.out.printf("%d*4=%d",c,c*4).println();
+        System.out.printf("%d*5=%d",c,c*5).println();
+        System.out.printf("%d*6=%d",c,c*6).println();
+        System.out.printf("%d*7=%d",c,c*7).println();
+        System.out.printf("%d*8=%d",c,c*8).println();
+
     }
 }
